f62d0a91c506e93a12af68a9245ff2a6f7190eda
MEG HOWE <howem9@gmail.com>
2021-07-14 13:46:43 -0400

    It works now, also added load todos

--- index.js
+++ index.js
@@ -1,81 +1,30 @@
-/* global Vue */
+/* global Vue, axios */
 
 var app = new Vue({
   el: "#app",
-  data: function() {
+  data: function () {
     return {
-      message: "Hellooooooo js",
+      message: "Hello from JavaScript!",
+      name: "Coconut",
       showInfo: false,
-      fruits: ["not strawberry", "pumpkin", "apple"],
+      fruits: ["apple", "banana", "orange"],
       newFruit: "",
+      todos: [],
     };
   },
   methods: {
     changeMessage: function () {
-      this.message = "Heyy";
+      this.message = "Whoa...";
     },
     addNewFruit: function () {
       this.fruits.push(this.newFruit);
       this.newFruit = "";
     },
+    loadTodos: function () {
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log(response.data);
+        this.todos = response.data;
+      });
+    },
   },
 });
-
-var app2 = new Vue({
-  el: '#app2',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
-
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true
-  }
-});
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-});
-
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
-  }
-});
-
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Fun on a bun.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-});
-
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
-
-
-// // Define a new component called todo-item
-// Vue.component('todo-item', {
-//   template: '<li>This is a todo</li>'
-// }),
-
-// var app = new Vue(...);
